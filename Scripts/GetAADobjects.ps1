74b8786117b601e47e327a40328cee6b93a56529
ianlucena <63721103+ianlucena@users.noreply.github.com>
2021-02-26 18:25:15 -0300

    Update GetAADobjects.ps1

--- Scripts/GetAADobjects.ps1
+++ Scripts/GetAADobjects.ps1
@@ -1,22 +1,27 @@
-﻿$Credential = Get-Credential
+#Credencial para acesso dos módulos
+$Credential = Get-Credential
 Connect-AzureAD -Credential $Credential | out-null
 Connect-MsolService -Credential $Credential 
 
 Write-Host `n 
 
-Write-Host "O tenant " -ForegroundColor White -NoNewline; Write-Host (Get-AzureADTenantDetail | Select-Object -ExpandProperty DisplayName) -ForegroundColor Green -NoNewline; Write-Host "" [(Get-AzureADTenantDetail | Select-Object -Expand VerifiedDomains | Select-Object -ExpandProperty name -First 1)] -ForegroundColor Gray -NoNewline; Write-Host " possui:" -ForegroundColor White -NoNewline; Write-Host `n 
+#Qual tenant está sendo verificado
+Write-Host "O tenant " -ForegroundColor White -NoNewline; Write-Host (Get-AzureADTenantDetail | Select-Object -ExpandProperty DisplayName) -ForegroundColor Green -NoNewline; 
+Write-Host "" [(Get-AzureADTenantDetail | Select-Object -Expand VerifiedDomains | Select-Object -ExpandProperty name -First 1)] -ForegroundColor Gray -NoNewline;
+Write-Host " possui:" -ForegroundColor White -NoNewline; Write-Host `n 
 
-#Para realizar a contagem de usuário
+
+#Contagem de usuário
 $users = (get-msoluser).Count
 Write-Host "            $users usuários" `n
 
-#Para realizar a contagem de grupos
+#Contagem de grupos
 $groups = (get-msolgroup).Count
 Write-Host "            $groups grupos" `n
 
-#Para realizar a contagem de dispositivos 
+#Contagem de dispositivos 
 $devices = (get-msoldevice -all).Count
 Write-Host "            $devices dispositivos" `n 
 
-#Soma dos objetos encontrados
+#Soma dos objetos
 Write-Host Totalizando ($users + $groups + $devices) objetos `n -ForegroundColor Yellow -NoNewline
